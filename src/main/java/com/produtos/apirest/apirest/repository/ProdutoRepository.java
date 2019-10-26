b599aef218d056bfd6e7ded77189f5f49f15cc9d
Joao Bezerra <joaobezerra547@gmail.com>
2019-10-26 15:44:33 -0300

    Metodos de manipulação do banco
    criei metodos para manipular os dados do banco, como por exemplo, o metodo de salvaProduto

--- src/main/java/com/produtos/apirest/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/apirest/repository/ProdutoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.apirest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
+	
+	Produto findById(long id);
 }
